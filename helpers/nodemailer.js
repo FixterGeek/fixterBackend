c0be103f423044e6273bd023c72575f5462bea52
MefitHp <mefitDev@gmail.com>
2019-02-07 17:45:27 -0600

    Node mailer added

---
+++ helpers/nodemailer.js
@@ -0,0 +1,40 @@
+const nodemailer = require('nodemailer')
+var hbs = require('nodemailer-express-handlebars');
+var options = {
+  viewEngine: {
+    extname: '.hbs',
+    layoutsDir: 'views/email/',
+    defaultLayout: 'template',
+    partialsDir: 'views/partials/'
+  },
+  viewPath: '../views/email/',
+  extName: '.hbs'
+};
+
+
+const transport = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: '',
+    pass: ''
+  },
+})
+
+function welcomeMail(nombre, email, tel, msj) {
+  transport..use('compile', hbs(options))
+  transport.sendMail({
+    bcc: email,
+    subject: 'Fixter Info.',
+    from: `${email}`,
+    to: '`contacto@fixter.org`',
+    template: 'contact',
+    context: {
+      nombre,
+      email,
+      tel,
+      msj,
+    }
+  })
+}
+
+module.exports = { welcomeMail }
