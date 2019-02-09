2541f07c929912c8f1d0813f748d3c204dea08a9
Héctor BlisS <hectorbliss@MacBook-Pro-de-Hector.local>
2019-02-09 15:26:04 -0600

    signature

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  //res.render('index');
+  res.json({
+    developedBy: "FixterGeek",
+    year: 2019,
+    site: "www.fixter.org",
+    bliss: "t(*_*t)"
+  });
 });
-
 
 module.exports = router;
