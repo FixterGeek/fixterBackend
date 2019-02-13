252ad6850667069fec38916f1335ba0fa4f2a949
oswaldo  martinez <oswaldomartinez@MacBook-Pro-de-oswaldo.local>
2019-02-13 16:28:39 -0600

    checking if application exists

--- controllers/applications.js
+++ controllers/applications.js
@@ -16,8 +16,10 @@ controller.adminAll = async (req, res) => {
 controller.apply = async (req, res) => {
   // console.log("lol", req.body);
   req.body.user = req.user._id;
+  let exists = await App.findOne({user:req.user._id, course:req.body.course})
+  if(exists) return res.json({message:'Ya has aplicado'})
   let app = await App.create(req.body);
-  res.status(200).send(app);
+  res.status(200).json(app);
 };
 
 module.exports = controller;
