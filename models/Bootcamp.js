bd1f6cc9528eb8a2bed9d101ccc8b0a313009f5e
Bliss <Bliss@MacBook-Pro-de-Bliss.local>
2019-09-19 14:31:31 -0500

    testing virtuals

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+let Week = require('./Week')
 
 const bootCampSchema = new Schema({
   current: Boolean,
@@ -25,13 +26,20 @@ const bootCampSchema = new Schema({
   active: {
     type: Boolean,
     default: true
-  },
-  weeks: [{
-    type: Schema.Types.ObjectId,
-    ref: "Week"
-  }]
+  }
+  // weeks: [{
+  //   type: Schema.Types.ObjectId,
+  //   ref: "Week"
+  // }]
 }, {
-    timestamps: true
+  timestamps: true
+});
+
+bootCampSchema
+  .virtual('weeks').get(async function () {
+    let weeks = await Week.find({ bootcamp: this._id })
+    console.log("virtual: ", weeks)
+    return weeks
   });
 
 module.exports = mongoose.model("Bootcamp", bootCampSchema);
