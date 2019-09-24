fbeeecfe597266f23eb14c95e4f0dfe65aba05ea
BlisS <bliss@fixter.org>
2019-09-23 21:51:05 -0500

    token console

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -7,6 +7,7 @@ exports.checkIfUserOrNext = (req, res, next) => {
     req.query.token ||
     req.headers["x-access-token"] ||
     req.headers["authorization"];
+  console.log(token)
   if (!token) return next()
   jwt.verify(token, process.env.SECRET, (err, decoded) => {
     if (err) return res.status(401).json({ message: "Token is invalid or has expired 👀" });
