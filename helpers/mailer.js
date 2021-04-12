25ded058ec5ce39a88d706b259e50631906cb3bb
Héctor BlisS <fixtergeek@gmail.com>
2021-04-11 21:13:19 -0500

    sell email template

--- helpers/mailer.js
+++ helpers/mailer.js
@@ -1,135 +1,135 @@
-let nodemailer = require("nodemailer");
-let hbs = require("hbs");
-let fs = require("fs");
+let nodemailer = require('nodemailer')
+let hbs = require('hbs')
+let fs = require('fs')
 
 let transport = nodemailer.createTransport({
-	service: "Gmail",
-	auth: {
-		user: process.env.NODEMAILER_USER,
-		pass: process.env.NODEMAILER_PASS,
-	},
-});
+  service: 'Gmail',
+  auth: {
+    user: process.env.NODEMAILER_USER,
+    pass: process.env.NODEMAILER_PASS,
+  },
+})
 
 const sellPromotion = hbs.compile(
-	fs.readFileSync((__dirname, "./views/mail/sell.hbs"), "utf8")
-);
+  fs.readFileSync((__dirname, './views/mail/sell.hbs'), 'utf8'),
+)
 
 const inscriptionToBootcamp = hbs.compile(
-	fs.readFileSync((__dirname, "./views/mail/inscripcion.hbs"), "utf8")
-);
+  fs.readFileSync((__dirname, './views/mail/inscripcion.hbs'), 'utf8'),
+)
 
 const accountCreated = hbs.compile(
-	// fs.readFileSync((__dirname, "./views/mail/bienvenida.hbs"), "utf8")
-	fs.readFileSync((__dirname, "./views/mail/welcome.hbs"), "utf8")
-);
+  // fs.readFileSync((__dirname, "./views/mail/bienvenida.hbs"), "utf8")
+  fs.readFileSync((__dirname, './views/mail/welcome.hbs'), 'utf8'),
+)
 const applyDone = hbs.compile(
-	fs.readFileSync((__dirname, "./views/mail/apply.hbs"), "utf8")
-);
+  fs.readFileSync((__dirname, './views/mail/apply.hbs'), 'utf8'),
+)
 
 const contactForm = hbs.compile(
-	fs.readFileSync((__dirname, "./views/mail/contactForm.hbs"), "utf8")
-);
+  fs.readFileSync((__dirname, './views/mail/contactForm.hbs'), 'utf8'),
+)
 
 exports.contactFormHorizon = ({
-	email,
-	name,
-	tel,
-	message,
-	date = Date.now(),
+  email,
+  name,
+  tel,
+  message,
+  date = Date.now(),
 }) => {
-	transport
-		.sendMail({
-			subject: "👾🤖¡Tienes un nuevo mensaje!😎",
-			bcc: ["gmonroyh75@gmail.com", "brenda@fixter.org"],
-			html: `<h3>¡Tienes un nuevo mensaje!</h3>
+  transport
+    .sendMail({
+      subject: '👾🤖¡Tienes un nuevo mensaje!😎',
+      bcc: ['gmonroyh75@gmail.com', 'brenda@fixter.org'],
+      html: `<h3>¡Tienes un nuevo mensaje!</h3>
 			<p> Nombre: ${name} </p>
 			<p> Correo: ${email} </p>
 			<p> Teléfono: ${tel}  </p>
 			<p>Mensaje: ${message} </p>
 			`,
-		})
-		.then((r) => {
-			console.log(r);
-			//return transport
-			// .sendMail({
-			// 	subject: "👾🤖¡Alguien nos ha contactado!😎",
-			// 	bcc: ["contacto@fixter.org", "brenda@fixter.org"],
-			// 	html: `
-			// 	<h2> Fecha: ${new Date()} </h2>
-			// 		<h2> Nombre: ${displayName} </h2>
-			// 		<h2> Teléfono: ${tel} </h2>
-			// 		<h2> Email: ${email} </h2>
-			// 		<h2> Mensaje: ${text} </h2>
+    })
+    .then((r) => {
+      console.log(r)
+      //return transport
+      // .sendMail({
+      // 	subject: "👾🤖¡Alguien nos ha contactado!😎",
+      // 	bcc: ["contacto@fixter.org", "brenda@fixter.org"],
+      // 	html: `
+      // 	<h2> Fecha: ${new Date()} </h2>
+      // 		<h2> Nombre: ${displayName} </h2>
+      // 		<h2> Teléfono: ${tel} </h2>
+      // 		<h2> Email: ${email} </h2>
+      // 		<h2> Mensaje: ${text} </h2>
 
-			// 	`
-			// })
-		})
-		.catch((e) => console.log(e));
-};
+      // 	`
+      // })
+    })
+    .catch((e) => console.log(e))
+}
 
 exports.sellAndPromotion = (emails) => {
-	return transport
-		.sendMail({
-			from: "fixtermailer@gmail.com",
-			subject: "👾 Nuevo bootcamp híbrido 🤖",
-			bcc: [emails],
-			html: sellPromotion({})
-		})
-		.then((r) => {
-			console.log(r);
-		})
-		.catch((e) => console.log(e));
-};
+  return transport
+    .sendMail({
+      from: 'fixtermailer@gmail.com',
+      subject: '👾 Nuevo bootcamp híbrido 🤖',
+      bcc: [emails],
+      html: sellPromotion({}),
+    })
+    .then((r) => {
+      console.log(r)
+    })
+    .catch((e) => console.log(e))
+}
 
 exports.inscriptionMail = ({ email, bootcampTitle, displayName }) => {
-	transport
-		.sendMail({
-			subject: "👾🤖¡Bienvenido al Bootcamp!😎",
-			bcc: email,
-			html: inscriptionToBootcamp({ name: bootcampTitle, displayName }),
-		})
-		.then((r) => console.log(r))
-		.catch((e) => console.log(e));
-};
+  transport
+    .sendMail({
+      subject: '👾🤖¡Bienvenido al Bootcamp!😎',
+      bcc: email,
+      html: inscriptionToBootcamp({ name: bootcampTitle, displayName }),
+    })
+    .then((r) => console.log(r))
+    .catch((e) => console.log(e))
+}
 
 exports.welcomeMail = ({ email, displayName }) => {
-	transport
-		.sendMail({
-			subject: "👾🤖¡Bienvenido a FixterGeek!😎",
-			bcc: email,
-			html: accountCreated({ name: displayName }),
-		})
-		.then((r) => console.log(r))
-		.catch((e) => console.log(e));
-};
+  transport
+    .sendMail({
+      subject: '👾🤖¡Bienvenido a FixterGeek!😎',
+      bcc: email,
+      html: accountCreated({ name: displayName }),
+    })
+    .then((r) => console.log(r))
+    .catch((e) => console.log(e))
+}
 
-exports.paymentMethods = ({ email, displayName = "Geek!" }, extraMail) => {
-	transport
-		.sendMail({
-			subject: "👾🤖¡Solo falta un paso más!😎",
-			bcc: [email, extraMail],
-			html: applyDone({ name: displayName }),
-		})
-		.then((r) => console.log(r))
-		.catch((e) => console.log(e));
-};
+exports.paymentMethods = ({ email, displayName = 'Geek!' }, extraMail) => {
+  transport
+    .sendMail({
+      subject: '👾🤖¡Solo falta un paso más!😎',
+      bcc: [email, extraMail],
+      html: applyDone({ name: displayName }),
+    })
+    .then((r) => console.log(r))
+    .catch((e) => console.log(e))
+}
 
 exports.contactFormReceived = (
-	{ email, displayName = "Geek!", text, tel },
-	extraMail
+  { email, displayName = 'Geek!', text, tel },
+  extraMail,
 ) => {
-	transport
-		.sendMail({
-			subject: "👾🤖¡Gracias por contactarnos!😎",
-			bcc: [email, extraMail],
-			html: contactForm({ name: displayName }),
-		})
-		.then((r) => {
-			console.log(r);
-			return transport.sendMail({
-				subject: "👾🤖¡Alguien nos ha contactado!😎",
-				bcc: ["contacto@fixter.org", "brenda@fixter.org"],
-				html: `
+  transport
+    .sendMail({
+      subject: '👾🤖¡Gracias por contactarnos!😎',
+      bcc: [email, extraMail],
+      html: contactForm({ name: displayName }),
+    })
+    .then((r) => {
+      console.log(r)
+      return transport.sendMail({
+        subject: '👾🤖¡Alguien nos ha contactado!😎',
+        bcc: ['contacto@fixter.org', 'brenda@fixter.org'],
+        html: `
 				<h2> Fecha: ${new Date()} </h2>
 					<h2> Nombre: ${displayName} </h2>
 					<h2> Teléfono: ${tel} </h2>
@@ -137,17 +137,17 @@ exports.contactFormReceived = (
 					<h2> Mensaje: ${text} </h2>
 					
 				`,
-			});
-		})
-		.catch((e) => console.log(e));
-};
+      })
+    })
+    .catch((e) => console.log(e))
+}
 
 exports.contactFormArango = ({ email, name, budget, tel, numPeople, date }) => {
-	transport
-		.sendMail({
-			subject: "👾🤖¡Tienes un nuevo mensaje!😎",
-			bcc: ["eventos@arangorestaurante.com"],
-			html: `<h3>¡Tienes un nuevo mensaje!</h3>
+  transport
+    .sendMail({
+      subject: '👾🤖¡Tienes un nuevo mensaje!😎',
+      bcc: ['eventos@arangorestaurante.com'],
+      html: `<h3>¡Tienes un nuevo mensaje!</h3>
 			<p> Nombre: ${name} </p>
 			<p> Correo: ${email} </p>
 			<p> Teléfono: ${tel}  </p>
@@ -155,41 +155,41 @@ exports.contactFormArango = ({ email, name, budget, tel, numPeople, date }) => {
 			<p> Presupuesto por persona: ${budget} </p>
 			<p> Fecha: ${date} </p>
 			`,
-		})
-		.then((r) => {
-			console.log(r);
-			//return transport
-			// .sendMail({
-			// 	subject: "👾🤖¡Alguien nos ha contactado!😎",
-			// 	bcc: ["contacto@fixter.org", "brenda@fixter.org"],
-			// 	html: `
-			// 	<h2> Fecha: ${new Date()} </h2>
-			// 		<h2> Nombre: ${displayName} </h2>
-			// 		<h2> Teléfono: ${tel} </h2>
-			// 		<h2> Email: ${email} </h2>
-			// 		<h2> Mensaje: ${text} </h2>
+    })
+    .then((r) => {
+      console.log(r)
+      //return transport
+      // .sendMail({
+      // 	subject: "👾🤖¡Alguien nos ha contactado!😎",
+      // 	bcc: ["contacto@fixter.org", "brenda@fixter.org"],
+      // 	html: `
+      // 	<h2> Fecha: ${new Date()} </h2>
+      // 		<h2> Nombre: ${displayName} </h2>
+      // 		<h2> Teléfono: ${tel} </h2>
+      // 		<h2> Email: ${email} </h2>
+      // 		<h2> Mensaje: ${text} </h2>
 
-			// 	`
-			// })
-		})
-		.catch((e) => console.log(e));
-};
+      // 	`
+      // })
+    })
+    .catch((e) => console.log(e))
+}
 
 exports.sendPasswordChanged = (password, email, name) => {
-	return transport
-		.sendMail({
-			from: "fixtermailer@gmail.com",
-			subject: "👾🤖 Recupera tu cuenta! ",
-			bcc: [email],
-			html: `
+  return transport
+    .sendMail({
+      from: 'fixtermailer@gmail.com',
+      subject: '👾🤖 Recupera tu cuenta! ',
+      bcc: [email],
+      html: `
 			<h2> Tu password provisional es: ${password} </h2>
 			<p> Puedes usarlo para acceder a la plataforma, pero no olvides cambiarlo inmediatamente por uno nuevo. </p>
 			<a href="https://www.fixter.camp/profile">Entra aquí</a>
 			<p>Nos vemos ;)</p>
 		`,
-		})
-		.then((r) => {
-			console.log(r);
-		})
-		.catch((e) => console.log(e));
-};
+    })
+    .then((r) => {
+      console.log(r)
+    })
+    .catch((e) => console.log(e))
+}
--- routes/mailRoutes.js
+++ routes/mailRoutes.js
@@ -1,50 +1,53 @@
-const express = require("express");
-const router = express.Router();
-const MailItem = require("../models/MailItem");
+const express = require('express')
+const router = express.Router()
+const MailItem = require('../models/MailItem')
 const User = require('../models/User')
-const { promisePaginator } = require("../helpers/paginator");
+const { promisePaginator } = require('../helpers/paginator')
 
 const {
-	contactFormReceived,
-	contactFormArango,
-	contactFormHorizon,
-	sellAndPromotion,
-} = require("../helpers/mailer");
+  contactFormReceived,
+  contactFormArango,
+  contactFormHorizon,
+  sellAndPromotion,
+} = require('../helpers/mailer')
 
 //5c6a4f9c79c3a60017bbeb65
 
-router.post("/hibrid", (req, res) => {
-	promisePaginator(req.body, User.find({_id:{$in:['5c6a4f9c79c3a60017bbeb65', '5d6361d2808c1c0017726001']}}) )
-	.then(({items, ...result})=>{
-		sellAndPromotion(items.map(item=>item.email))
-		res.status(200).send(result);
-	})
-	
-});
+router.post('/hibrid', (req, res) => {
+  promisePaginator(
+    req.body,
+    User.find({
+      _id: { $in: ['5c6a4f9c79c3a60017bbeb65', '5d6361d2808c1c0017726001'] },
+    }),
+  ).then(({ items, ...result }) => {
+    sellAndPromotion(items.map((item) => item.email))
+    res.status(200).send(result)
+  })
+})
 
-router.post("/horizonte/contacto", (req, res, next) => {
-	if (req.body.token !== "noLine")
-		return res.status(403).send({ message: "Forbiden" });
-	contactFormHorizon(req.body);
-	res.status(200).send({ message: "Sending" });
-});
+router.post('/horizonte/contacto', (req, res, next) => {
+  if (req.body.token !== 'noLine')
+    return res.status(403).send({ message: 'Forbiden' })
+  contactFormHorizon(req.body)
+  res.status(200).send({ message: 'Sending' })
+})
 
-router.post("/arango/contacto", (req, res, next) => {
-	if (req.body.token !== "cocinaDeRaices")
-		return res.status(403).send({ message: "Forbiden" });
-	contactFormArango(req.body);
-	res.status(200).send({ message: "Sending" });
-});
+router.post('/arango/contacto', (req, res, next) => {
+  if (req.body.token !== 'cocinaDeRaices')
+    return res.status(403).send({ message: 'Forbiden' })
+  contactFormArango(req.body)
+  res.status(200).send({ message: 'Sending' })
+})
 
-router.post("/contacto", (req, res, next) => {
-	//console.log("body", req.body)
-	contactFormReceived(req.body);
-	res.status(200).send({ message: "sending" });
-});
+router.post('/contacto', (req, res, next) => {
+  //console.log("body", req.body)
+  contactFormReceived(req.body)
+  res.status(200).send({ message: 'sending' })
+})
 
-router.post("/maillist", async (req, res) => {
-	let mItem = await MailItem.create(req.body);
-	res.status(200).send({ message: "Mail added", mItem });
-});
+router.post('/maillist', async (req, res) => {
+  let mItem = await MailItem.create(req.body)
+  res.status(200).send({ message: 'Mail added', mItem })
+})
 
-module.exports = router;
+module.exports = router
