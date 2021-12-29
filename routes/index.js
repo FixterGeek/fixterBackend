02b3b4ac99ec026ce3e82837ef9a491f7b23b5bd
hectorbliss <fixtergeek@gmail.com>
2021-12-29 11:21:18 -0600

    webhook

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,10 @@ const { sendDisciplineChallenge } = require("../helpers/mailer");
 const router = express.Router();
 const stripe = require('stripe')('sk_test_51K6dXmJ7Zwl77LqntfyjDm7s6ZFZYuiCB2G00swjcN8VzyYsZZfFiWOfYcMnveiixSaVtYsqdCPipWAonEMCaREy00rG91msfD');
 
-// const CLIENT_DOMAIN = 'http://localhost:3000/pricing';
-const CLIENT_DOMAIN = 'https://fixter.camp/pricing';
-const SERVER_DOMAIN = 'https://fixtercamp.herokuapp.com'
-// const SERVER_DOMAIN = 'http://localhost:8000'
+const CLIENT_DOMAIN = 'http://localhost:3000/pricing';
+// const CLIENT_DOMAIN = 'https://fixter.camp/pricing';
+// const SERVER_DOMAIN = 'https://fixtercamp.herokuapp.com'
+const SERVER_DOMAIN = 'http://localhost:8000'
 
 /* GET home page */
 router.get("/", (req, res, next) => {
@@ -80,49 +80,81 @@ router.get('/billing', verifyToken, async (req, res) => {
     return_url: returnUrl,
   }).then(portalSession => {
     res.redirect(303, portalSession.url);
-
   }).catch(() => {
     res.status(404)
   })
 })
 
+// Webhooks
+const endpointSecret = "whsec_x39WEogZp9UwYBfL8Kz0UxSrderL43TZ";
+router.get('/webhook', async (req, res) => {
+
+  const sig = req.headers['stripe-signature'];
+  let event;
+  try {
+    event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
+  } catch (err) {
+    response.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+  // Handle the event
+  switch (event.type) {
+    case 'payment_intent.succeeded':
+      const paymentIntent = event.data.object;
+      console.log(event)
+      // Then define and call a function to handle the event payment_intent.succeeded
+      break;
+    case 'subscription_schedule.updated':
+      console.log("all event: ", event)
+      console.log("uopdate: => ", event.data.object)
+    // ... handle other event types
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+      console.log(`Todo: ${event}`);
+      console.log(`Data: ${event.data.object}`);
+  }
+
+  // Return a 200 response to acknowledge receipt of the event
+  response.send();
+})
+
 // mail
-router.get("/blissi", (req, res, next) => {
-  //res.render('index');
-  const emailsOne = [
-    'lgabrich@hotmail.com',
-    'pcromero2207@hotmail.com',
-    'rockocb12@gmail.com',
-    'magetzyestrada5@gmail.com',
-    'marijotl_12@hotmail.com',
-    'rflores@surveyup.com.mx',
-    'tesah74@gmail.com',
-    'hubencam0907@gmail.com',
-    'covaale457@gmail.com',
-    'rodrigopi51.49@gmail.com',
-    'behec.dev@gmail.com',
-    'alfonsoomaster@gmail.com',
-    'carlamafc@gmail.com',
-    'rahelish@hotmail.com',
-    'yolanda.rochaa90@gmail.com',
-    'michaelcastillo.pe@hotmail.com',
-    'marcarrimor@gmail.com',
-    'fixtergeek@gmail.com',
-  ] // sent Dic 21th 2021
-  const emailsTwo = [
-    'blanca.aloma7@gmail.com',
-    'yann.mhdz@gmail.com',
-    'tesah74@gmail.com'
-  ] // sent...
-  const emailsThree = ['pcromero2207@hotmail.com']
-  sendDisciplineChallenge([])
-  res.json({
-    developedBy: "FixterGeek",
-    year: 2019,
-    site: "www.fixter.org",
-    bliss: "t(*_*t)",
-    message: "Enviando correos"
-  });
-});
+// router.get("/blissi", (req, res, next) => {
+//   //res.render('index');
+//   const emailsOne = [
+//     'lgabrich@hotmail.com',
+//     'pcromero2207@hotmail.com',
+//     'rockocb12@gmail.com',
+//     'magetzyestrada5@gmail.com',
+//     'marijotl_12@hotmail.com',
+//     'rflores@surveyup.com.mx',
+//     'tesah74@gmail.com',
+//     'hubencam0907@gmail.com',
+//     'covaale457@gmail.com',
+//     'rodrigopi51.49@gmail.com',
+//     'behec.dev@gmail.com',
+//     'alfonsoomaster@gmail.com',
+//     'carlamafc@gmail.com',
+//     'rahelish@hotmail.com',
+//     'yolanda.rochaa90@gmail.com',
+//     'michaelcastillo.pe@hotmail.com',
+//     'marcarrimor@gmail.com',
+//     'fixtergeek@gmail.com',
+//   ] // sent Dic 21th 2021
+//   const emailsTwo = [
+//     'blanca.aloma7@gmail.com',
+//     'yann.mhdz@gmail.com',
+//     'tesah74@gmail.com'
+//   ] // sent...
+//   const emailsThree = ['pcromero2207@hotmail.com']
+//   sendDisciplineChallenge([])
+//   res.json({
+//     developedBy: "FixterGeek",
+//     year: 2019,
+//     site: "www.fixter.org",
+//     bliss: "t(*_*t)",
+//     message: "Enviando correos"
+//   });
+// });
 
 module.exports = router;
