83a41044c8eafa77b97d6d5202f5c07166231b89
hectorbliss <fixtergeek@gmail.com>
2021-12-29 11:24:22 -0600

    updated to post

--- routes/index.js
+++ routes/index.js
@@ -87,7 +87,7 @@ router.get('/billing', verifyToken, async (req, res) => {
 
 // Webhooks
 const endpointSecret = "whsec_dNelZ1O6JOwQHGWmRX8h7bUNnkHKwKzg";
-router.get('/webhook', async (req, res) => {
+router.post('/webhook', async (req, res) => {
 
   const sig = req.headers['stripe-signature'];
   let event;
